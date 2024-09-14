d951073f1154ab2352435cc5aeda6dc4d9de9993
jiangxiaoyu07 <jiangxiaoyu07@kuaishou.com>
2024-09-14 17:17:18 +0800

    feat:machinery

---
+++ example/machinery/getting-started/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
+	"log"
+	"time"
+)
+
+func main() {
+
+	cnf, err := config.NewFromYaml("./config.yml", false)
+	if err != nil {
+		log.Println("config failed", err)
+		return
+	}
+
+	server, err := machinery.NewServer(cnf)
+	if err != nil {
+		log.Println("start server failed", err)
+		return
+	}
+
+	// 注册任务
+	err = server.RegisterTask("sum", Sum)
+	if err != nil {
+		log.Println("reg task failed", err)
+		return
+	}
+
+	worker := server.NewWorker("asong", 1)
+	go func() {
+		err = worker.Launch()
+		if err != nil {
+			log.Println("start worker error", err)
+			return
+		}
+	}()
+
+	//task signature
+	signature := &tasks.Signature{
+		Name: "sum",
+		Args: []tasks.Arg{
+			{
+				Type:  "[]int64",
+				Value: []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			},
+		},
+	}
+
+	asyncResult, err := server.SendTask(signature)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := asyncResult.Get(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("get res is %v\n", tasks.HumanReadableResults(res))
+
+}
+
+func Sum(args []int64) (int64, error) {
+	sum := int64(0)
+	for _, arg := range args {
+		sum += arg
+	}
+
+	return sum, tasks.NewErrRetryTaskLater("我说他错了", 4*time.Second)
+
+}
